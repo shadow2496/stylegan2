8a60f37a21222677a9b9d481c20761acf1cada0e
Seunghwan Choi <dadadak4@gmail.com>
2020-08-03 22:27:50 +0900

    Update training options in run()

--- run_training.py
+++ run_training.py
@@ -89,7 +89,11 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, resolution, total_ki
     train.data_dir = data_dir
     train.total_kimg = total_kimg
     train.mirror_augment = mirror_augment
-    train.image_snapshot_ticks = train.network_snapshot_ticks = 10
+    train.image_snapshot_ticks = 10
+    train.network_snapshot_ticks = 125
+    # train.resume_pkl = None
+    # train.resume_kimg = 0.0
+    # train.resume_time = 0.0
     sched.G_lrate_base = sched.D_lrate_base = 0.002
     sched.minibatch_size_base = 32
     sched.minibatch_gpu_base = 4
